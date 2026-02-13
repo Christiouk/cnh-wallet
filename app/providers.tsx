35113c2979c0f10045c8b628fbbc977d4b077404
Christiouk <c.mail@me.com>
2026-02-13 20:03:08 +0000

    Update providers.tsx

--- app/providers.tsx
+++ app/providers.tsx
@@ -2,7 +2,11 @@
 
 import { PrivyProvider } from '@privy-io/react-auth';
 
-export default function Providers({ children }: { children: React.ReactNode }) {
+export default function Providers({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <PrivyProvider
       appId={process.env.NEXT_PUBLIC_PRIVY_APP_ID!}
